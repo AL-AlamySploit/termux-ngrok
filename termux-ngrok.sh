8d67b6829abbe3e2616a4ad406d565062985d8fe
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-09-08 00:19:01 +0200

    Update termux-ngrok.sh

--- termux-ngrok.sh
+++ termux-ngrok.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/bash
 #Developed by: Ahmed Mohamed (A l - A l a m y)
 #YouTube Channel: https://www.youtube.com/channel/UCQuGjfmo04jDd6zlBscslGQ
 #YouTube Channel: https://www.youtube.com/channel/UCm-UlQ6ygk4jkNfgFzlc2LA
@@ -11,20 +11,19 @@ b='\e[99;34m' # Blue
 
 clear
 echo
-echo "${b} █████╗ ██╗       █████╗ ██╗      █████╗ ███╗   ███╗██╗   ██╗";
-echo "${b}██╔══██╗██║      ██╔══██╗██║     ██╔══██╗████╗ ████║╚██╗ ██╔╝";
-echo "${b}███████║██║█████╗███████║██║     ███████║██╔████╔██║ ╚████╔╝";
-echo "${b}██╔══██║██║╚════╝██╔══██║██║     ██╔══██║██║╚██╔╝██║  ╚██╔╝";
-echo "${b}██║  ██║███████╗ ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║   ██║";
-echo "${b}╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝";
+echo -e "${b} █████╗ ██╗       █████╗ ██╗      █████╗ ███╗   ███╗██╗   ██╗";
+echo -e "${b}██╔══██╗██║      ██╔══██╗██║     ██╔══██╗████╗ ████║╚██╗ ██╔╝";
+echo -e "${b}███████║██║█████╗███████║██║     ███████║██╔████╔██║ ╚████╔╝";
+echo -e "${b}██╔══██║██║╚════╝██╔══██║██║     ██╔══██║██║╚██╔╝██║  ╚██╔╝";
+echo -e "${b}██║  ██║███████╗ ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║   ██║";
+echo -e "${b}╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝";
 echo
-echo $g"Do you want to install Ngrok? [Y/n]"
+echo -e $g"Do you want to install Ngrok? [Y/n]"
 read selection
 case $selection in
 y)
+    clear
     echo
-    xdg-open https://www.youtube.com/channel/UCm-UlQ6ygk4jkNfgFzlc2LA
-    echo $g"Downloading..!"
     sleep 0.4
     #printf "Please wait..[                    ] 0% "
     #sleep 0.4
@@ -57,30 +56,37 @@ x86_64)
 *)
     echo "unknown architecture"
 esac
-
+echo -e $g"Downloading..!"
 wget "https://github.com/AL-AlamySploit/Ngrok-Test/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
 unzip ngrok.zip
 cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
 chmod 700 /data/data/com.termux/files/usr/bin/ngrok
 rm ngrok ngrok.zip
+xdg-open https://www.youtube.com/channel/UCm-UlQ6ygk4jkNfgFzlc2LA
+sleep 0.10
 clear
-echo "${g}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
-echo "${g}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
-echo "${g}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
-echo "${g}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
-echo "${g}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
-echo "${g}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo -e "${g}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo -e "${g}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo -e "${g}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo -e "${g}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo -e "${g}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo -e "${g}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
 echo "                  v2.0                          "
 echo
-echo $g"Example driving ('ngrok http 80'\n or ngrok help)"
+echo -e $g"Example driving ('ngrok http 80'\n or ngrok help)"
 ;;
 
 n)
     clear
-    echo "${r}[-] Ngrok not installed"
+    echo -e "${r}[-] Ngrok not installed"
     echo
     ;;
+    
+exit)
+    echo -e $y"Goodbye "
+    ;;
+    
 *)
-    echo "unknown"
+    echo -e $y"unknown"
     ;;
 esac
